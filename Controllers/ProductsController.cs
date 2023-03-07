d141f9e8b0a48b67e37d1b05ec185223efbb34a2
MinhQuan9812 <minwonasd123@gmail.com>
2023-03-07 21:34:04 +0700

    .net

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using testdocnet.Repositories;
+
+namespace testdocnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public readonly IBookRepository _bookRepo;
+
+        public ProductsController(IBookRepository repo)
+        {
+            _bookRepo = repo;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            try
+            {
+                return Ok(await _bookRepo.GetAllBooksAsync());
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+
+}
+
